5cf191d5a406d623c7f88918debc12df0d36f123
Nicolas Chantreau <citrouille86@gmail.com>
2019-10-07 16:23:18 +0200

    Move, refactor and rename old repo

---
+++ src/main/java/fr/nchantreau/SpringBatchParallelApplication.java
@@ -0,0 +1,12 @@
+package fr.nchantreau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchParallelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchParallelApplication.class, args);
+    }
+}
